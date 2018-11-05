2c4111a230857b1876f86c24f499f7311555306b
Kenshou Uemura <u3mur4@gmail.com>
2018-11-05 15:06:37 +0100

    change international.bittrex.com

--- history_bar.js
+++ history_bar.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://bittrex.com/Market/Index?MarketName=*-*
+// @match        https://international.bittrex.com/Market/Index?MarketName=*-*
 // @grant        none
 // ==/UserScript==
 
